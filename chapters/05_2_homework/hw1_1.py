49830df4704a1a1361d838664f82fe78f74312f2
kyla <kylahare2017@gmail.com>
2025-10-16 10:00:22 -0400

    week 9 class work

--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -16,15 +16,18 @@ def print_tic_tac_toe():
 
 
 # print("********** Homework 1 Exercise 2 **********")
-def you_name():
-    
+def you_name(): 
+
+    first=input('Enter your first name: ')
+    second=input('Enter your last name: ')
+    third=input('Month? ')
+    fourth=input('day? ') 
+    fith=input('year?')
+    print(first, second, 'was born', third, fourth, fith,)
+# you_name()
+
+
 
 # print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
 
 
-
-# print("********** Homework 1 Exercise 3 **********")
-
-# # Do your work for Excercise 3 here
-
-# print("Homework 1 Exercise 3: Not implemented") # Delete this line when you write your code!
---
+++ chapters/05_2_homework/rsp.py
@@ -0,0 +1,7 @@
+def rock_rules():
+    if (player1 == 'rock' and player2 == 'scissors'):
+        print('player 1 wins.')
+    elif (player1 == 'paper' and player2 == 'rock')
+        print('player 1 wins.')
+    elif (player1 == 'scissors' and player2 == 'paper')
+        print('player 1 wins') 
