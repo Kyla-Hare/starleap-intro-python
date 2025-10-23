65698987446afeec3c2f9a22b40ec4608bb37f1a
kyla <kylahare2017@gmail.com>
2025-10-23 03:07:26 -0400

    week 10

--- chapters/05_2_homework/rsp.py
+++ chapters/05_2_homework/rsp.py
@@ -1,7 +1,26 @@
-def rock_rules():
+import pwinput 
+
+
+
+def game_rules(player1, player2):
+    
     if (player1 == 'rock' and player2 == 'scissors'):
         print('player 1 wins.')
-    elif (player1 == 'paper' and player2 == 'rock')
+    elif (player1 == 'paper' and player2 == 'rock'):
         print('player 1 wins.')
-    elif (player1 == 'scissors' and player2 == 'paper')
-        print('player 1 wins') 
+    elif (player1 == 'scissors' and player2 == 'paper'):
+        print('player 1 wins')
+
+    elif(player1 == player2):
+        print('you all loose')
+    else:
+        print('player2 wins') 
+# def validate(val):
+
+def main():
+    player1 = input('player1? ')
+    # validate(player1) 
+    player2 = input('Player2? ') 
+    
+    game_rules(player1, player2) 
+main()
--- chapters/06_1_lesson/exercises.py
+++ chapters/06_1_lesson/exercises.py
@@ -2,39 +2,50 @@
 ##### Template for Chapter 5.14, Exercises 1 - 4 ######
 
 
-print("********** Ch 6 Exercise 1 **********")
+# print("********** Ch 6 Exercise 1 **********")
 
-print("""
-Type your work for Exercise 1 here
+def b(z):
+    prod = a(z, z)
+    print(z, prod)
+    return prod
 
-Ch 6 Exercise 1: Not implemented # Delete this line when you write your code!
-""")
+def a(x, y):
+    x = x + 1
+    return x * y
+
+def c(x, y, z):
+    total = x + y + z
+    square = b(total)**2
+    return square
+
+x = 1
+y = x + 1
+# print(c(x, y+3, x+y))
 
 
-print("********** Ch 6 Exercise 2 **********")
-
-# Do your work for Excercise 2 here.
-
-print("Ch 6 Exercise 2: Not implemented") # Delete this line when you write your code!
+# print("********** Ch 6 Exercise 2 **********")
 
 
+# print("Ch 6 Exercise 2: Not implemented") # Delete this line when you write your code!
 
-print("********** Ch 6 Exercise 3 **********")
+
+
+# print("********** Ch 6 Exercise 3 **********")
 
 # Exercise 3 should be worked in a new file called palindrome.py
 
 
 
-print("********** Ch 6 Exercise 4 **********")
+# print("********** Ch 6 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
 
-print("Ch 6 Exercise 4: Not implemented") # Delete this line when you write your code!
+# print("Ch 6 Exercise 4: Not implemented") # Delete this line when you write your code!
 
 
 
-print("********** Ch 6 Exercise 5 **********")
+# print("********** Ch 6 Exercise 5 **********")
 
 # Do your work for Exercise 5 here.
 
-print("Ch 6 Exercise 5: Not implemented") # Delete this line when you write your code!
+# print("Ch 6 Exercise 5: Not implemented") # Delete this line when you write your code!
