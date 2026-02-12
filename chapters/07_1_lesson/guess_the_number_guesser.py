85e56a107f510b1e8f46676df6ed9901cc8d5dd6
Kyla <Kyla@Hareware.local>
2026-02-12 09:56:26 -0500

    Week 2 spring work

--- chapters/07_1_lesson/guess_the_number_guesser.py
+++ chapters/07_1_lesson/guess_the_number_guesser.py
@@ -4,29 +4,54 @@
 # The program should tell the user how many guesses it took to guess the number.
 
 import random
+MIN_NUMBER = 1
+MAX_NUMBER = 100
 
 
 def get_number_feedback():
-    # TODO: Implement this function
-    pass
+    # TODO: Implement this function 
+    answer = input("Enter 'h' if the guess is too high, 'l' if it's too low, 'c' if it's correct")
+    return answer 
+    
 
 
 def get_number():
     # TODO: Implement this function
-    pass
+    #return random.randint(MIN_NUMBER, MAX_NUMBER)
+    return (MIN_NUMBER + MAX_NUMBER) // 2
+    
 
 
 def play_guesser():
-    MIN_NUMBER = 1
-    MAX_NUMBER = 100
+    global MIN_NUMBER
+    global MAX_NUMBER
+    
     print('-' * 60)
     print()
     print(f"Think of a number between {MIN_NUMBER} and {MAX_NUMBER} (inclusive).")
     input("Press Enter when you have thought of a number.")
     print()
     guess_count = 0
-    # TODO: Implement the rest of this function
-    pass
+    # TODO: Implement the rest of this function 
+    while True: 
+        guess_count +=1
+        # computer decide guess 
+        guess = get_number()
+        print(f"I'm guessing {guess}")
+        # asker user for feed back on geuss 
+        feedback = get_number_feedback()
+        if feedback == 'c': 
+            print(f"I guessed your number in {guess_count} guesses") 
+            return guess_count 
+        elif feedback == 'h': 
+            MAX_NUMBER = guess - 1
+        elif feedback == 'l':
+            MIN_NUMBER = guess + 1
+            
+        # if correct exit function
+        # if incorrecr decide nect guess
+
+    
 
 def main():
     print('-' * 60)
@@ -36,7 +61,7 @@ def main():
     while True:
         guess_count = play_guesser()
         answer = input("Do you want to play again? (y/n) ").lower()
-        if answer == "n":
+        if answer != "y":
             print("Thanks for playing!")
             break
 
--- chapters/07_1_lesson/guess_the_number_picker.py
+++ chapters/07_1_lesson/guess_the_number_picker.py
@@ -2,16 +2,38 @@
 # The program thinks of a number between 1 and 100 and the user tries to guess it.
 # The program should tell the user if the guess is too high or too low.
 # The program should also tell the user how many guesses it took to guess the number.
-
+import random 
 MIN_NUMBER = 1
 MAX_NUMBER = 100
 
 def get_valid_guess():
-    # TODO: Implement this function
-    pass
+    while True: 
+        guess_text = input("GUESS A NUMNER:")
+        try: 
+            guess = int(guess_text)
+            if guess > MAX_NUMBER or guess < MIN_NUMBER:
+                raise ValueError()
+            return guess
+        except:
+            print("NOT ACCEPTABLE, guess again")
+    
+    
 
 def play_picker():
+    number = random.randint(MIN_NUMBER, MAX_NUMBER) 
+    print("computer picked {number}. shhh")
+    print(f"I've picked a munber between{MIN_NUMBER} and {MAX_NUMBER}")
+    while True:
+        guess = get_valid_guess()
+        if guess == number:
+            print("congrates you gussed right, finally")
+            break
+        elif guess > number:
+            print("To high") 
+        else:
+            print("Too low")
     # TODO: Implement this function
+    
     pass
 
 def main():
@@ -22,7 +44,7 @@ def main():
     while True:
         guess_count = play_picker()
         answer = input("Do you want to play again? (y/n) ").lower()
-        if answer == "n":
+        if answer != "y":
             print("Thanks for playing!")
             break
 
